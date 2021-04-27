89805b26441e02eef9a57e4baf225610f0b4d9f7
Brian Chau <bchau1000@gmail.com>
2021-04-26 18:11:20 -0700

    Initalized project file

---
+++ hw.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_MAIN 32
+#define MAX_VIRTUAL 64
+
+int main(int argc, char *argv[]) {
+    char *main_mem[MAX_MAIN] = {"-1"};
+    char *virtual_mem[MAX_VIRTUAL] = {"-1"};
+
+    if(argc > 1) {
+        
+    }
+    else {
+
+    }
+
+
+    return 0;
+}
